ea51f2e951550585dbe41ea1c643628f1ed02181
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-02-18 21:58:19 -0800

    memory match test

--- main.js
+++ main.js
@@ -1,280 +1,163 @@
 $(document).ready(initializeApp);
 
-var dataStorage = [{
-	value: '',
-	rank: '',
-}];
-var inputHistory = [];
-var operators = ['+', '-', '*', '/', 'sin', 'cos', 'power', 'root', 'log', 'factorial'];
-var ext_operators = ['sin', 'cos', 'power', 'root', 'log', 'factorial'];
-var calculated = false;
+var first_card_clicked = null;
+var match_counter = 0;
+var attempt_counter = 0;
+var anotherClickAllowed = true;
+var cardArr = ['img/card-1.png', 'img/card-1.png', 'img/card-2.png', 'img/card-2.png',
+			   'img/card-3.png', 'img/card-3.png', 'img/card-4.png', 'img/card-4.png',
+			   'img/card-5.png', 'img/card-5.png', 'img/card-6.png', 'img/card-6.png',
+			   'img/card-7.png', 'img/card-7.png', 'img/card-8.png', 'img/card-8.png',
+			   'img/card-9.png', 'img/card-9.png'];
 
 function initializeApp() {
-	addClickHandlers();
+	startCutScene();
+	setTable();
 }
 
-function addClickHandlers() {
-	$('.clear button').on('click', handleClearClick);
-	$('.operators button').on('click', handleOperatorClick);
-	$('.ext_operators button').on('click', handleOperatorClick);
-	$('.numbers button').on('click', handleNumberClick);
-	$('.ext_operators .intro').on('click', displayIntro);
+function setTable() {
+	shuffle(cardArr);
+	makeCards(cardArr);
+	var card = $('#game .card');
+	$(card).on('click', handleClick);
+	$('#game .stats .reset').on('click', reset);
 }
 
-function handleClearClick() {
-	if ($(this).text() === 'CE') {
-		dataStorage.pop();
-		if (dataStorage.length === 0) {
-			insert(dataStorage);
-		} 
-	}
-	if ($(this).text() === 'C') {
-		dataStorage = [];
-		insert(dataStorage);
-		inputHistory = [];
-		calculated = false;
-	}
-	updateDisplay(dataStorage);
-}
-
-function handleOperatorClick() {
-	var operatorClicked = $(this).attr('value');
-	var lastEntry = dataStorage[dataStorage.length-1];
-	calculated = false;
-	if ( operators.includes(lastEntry.value) && !ext_operators.includes(operatorClicked)) {
-			lastEntry.value = operatorClicked; // only stores the last basic operator, for multiple/changing operands
-	} else if ( lastEntry.value !== undefined){ 
-		insert(dataStorage,operatorClicked, 2);
-	}		
-	updateDisplay(dataStorage);
-}
-
-function handleNumberClick() {
-	var numberClicked = $(this).text();
-	var lastEntry = dataStorage[dataStorage.length-1];
-	if (numberClicked === '=') {
-		handleEquals(dataStorage);		
+function handleClick() {
+	if (first_card_clicked === null && anotherClickAllowed) {
+		first_card_clicked = this;
+		flipBack(this);
 	} else {
-		if (numberClicked === '.'){
-			if(lastEntry.value.toString().indexOf('.') === -1 && !operators.includes(lastEntry.value)) {
-				storeValue(dataStorage,lastEntry.value += numberClicked, 1);
-			} else if (operators.includes(lastEntry.value)){
-				insert(dataStorage,numberClicked, 1);
-			}
-		} else if (isNumeric(numberClicked)) { 
-			if (calculated) {
-				dataStorage = [];
-				insert(dataStorage);
-				lastEntry = dataStorage[dataStorage.length-1];
-				calculated = false;
-			} 
-			if ( lastEntry.value === undefined || !operators.includes(lastEntry.value)){
-			storeValue(dataStorage,lastEntry.value += numberClicked, 1);
-			} else if (operators.includes(lastEntry.value)){
-				insert(dataStorage,numberClicked, 1);
-			}
-		} 
-		updateDisplay(dataStorage);
+		if(!$(this).hasClass('flipper') && anotherClickAllowed ){
+			anotherClickAllowed= false;
+			var second_card_clicked = this;
+			flipBack(this);
+			attempt_counter++;
+			compareCards(first_card_clicked, second_card_clicked);
+		}
 	}
 }
 
-function isNumeric(num) {
-	return !isNaN(parseFloat(num)) && isFinite(num);
+function flipBack(card) {
+	$(card).addClass('flipper');
 }
 
-function handleEquals(inputData) {
-	var result;
-	if(!calculated){	
-		if (inputData[0].value === '' && inputData.length === 1 && inputHistory.length === 0) {		
-			// for 'missing operands'
-			var readyMsg = [];
-			insert(readyMsg, 'Ready', 1);
-			result = readyMsg;
-			inputData = [];
-			insert(inputData);
-			calculated = false;
-		} else if (inputData[0].value !== '' && inputData.length === 1 && inputHistory.length === 0) {
-			// for 'missing operation'
-			result = doMath(inputData);
-			calculated = true;
-		} else if(inputData.length === 2 && isNumeric(inputData[0].value) && operators.includes(inputData[1].value)){
-			//for 'partial operand'
-			duplicateLastNumber(inputData);
-			result = doMath(inputData);
-			calculated = true;
-		} else if (isNumeric(inputData[inputData.length-2].value) && operators.includes(inputData[inputData.length-1].value)) {
-			//for 'operation rollover'
-			format(inputData);
-			var lastOperand = JSON.parse(JSON.stringify(inputData.splice(inputData.length-1,1)));
-			doMath(inputData);
-			inputData.push(lastOperand[0]);
-			duplicateLastNumber(inputData);
-			result = doMath(inputData);
-			calculated = true;
-		}else {
-			inputHistory = JSON.parse(JSON.stringify(inputData));
-			format(inputData);
-			result = doMath(inputData);
-			calculated = true;
-		}
+function showBack(card) {
+	$(card).removeClass('flipper');
+}
+
+function compareCards(card1, card2) {
+	var cardName1 = $(card1).find('.front img').attr('src');
+	var cardName2 = $(card2).find('.front img').attr('src');
+	if (cardName1 === cardName2){
+		match_counter++;
+		updateStats(match_counter);
+		anotherClickAllowed = true;
+		first_card_clicked = null;  //no need to check if local variable second_card_clicked = null;
 	} else {
-		if (operators.includes(inputData[inputData.length-1].value) && !ext_operators.includes(inputData[inputData.length-1].value)){
-			duplicateLastNumber(inputData);
-			result = doMath(inputData);
-		} else if (!operators.includes(inputData[inputData.length-1].value)) {
-			//for 'operation repeat'
-			format(inputData);
-			var lastOperation = inputHistory.slice(inputHistory.length-2);
-			// Array.portotype.push.apply(inputData, lastOperation);
-			inputData.push(lastOperation[0]);
-			inputData.push(lastOperation[1]);
-			inputHistory = JSON.parse(JSON.stringify(inputData));
-			format(inputData);
-			result = doMath(inputData);
-		}
-		calculated = true;
-	}	
-	updateDisplay(result);
-}
-
-function duplicateLastNumber(inputData) {
-	var repeatData = JSON.parse(JSON.stringify(inputData[inputData.length-2]));
-	inputData.push(repeatData);
-	format(inputData);
-}
-
-function insert(inputData, value, rank) {
-	var itemToAdd = {};
-		itemToAdd.value = value || '';
-		itemToAdd.rank = rank || '';
-		inputData.push(itemToAdd);
-}
-
-function storeValue(inputData, value, rank) {
-		inputData[inputData.length-1].value = value;
-		inputData[inputData.length-1].rank = rank;
-}
-
-function getValue(inputData) {
-	var storedValue = [];
-	for (var i = 0; i < inputData.length; i++) {
-		storedValue.push(inputData[i].value);
-	}
-	return storedValue;
-}
-
-function format(inputData) {
-	// if (operators.includes(inputData[inputData.length-1].value)){
-	// 	inputData.pop();
-	// }
-	if (inputData[0].value == 0 && inputData[1].value !== '.') {
-		inputData.shift();
-	}
-	while (inputData[0].value === '' || operators.includes(inputData[0].value) && !ext_operators.includes(inputData[0].value)){
-		inputData.shift(); //for 'premature operation'
-	}
-	for (var i = 0; i < inputData.length; i++) {
-	 	if (inputData[i].value === '*' || inputData[i].value === '/'){
-	 	inputData[i].rank = 3;
-	 	} else if (inputData[i].value === 'power' || inputData[i].value === 'root') {
-	 	inputData[i].rank = 4;
-	 	}else if (inputData[i].value === 'sin' || inputData[i].value === 'cos' || inputData[i].value === 'log' || inputData[i].value === 'factorial') {
-	 	inputData[i].rank = 5;
-	 	} else if (!operators.includes(inputData[i].value)){
-	 		inputData[i].value = parseFloat(inputData[i].value);
-	 	}
+		setTimeout(function(){
+			showBack(card1);
+			showBack(card2);
+			setTimeout(function(){
+			anotherClickAllowed = true;
+			first_card_clicked = null;  //no need to check if second_card_clicked = null;
+			}, 1050);
+		}, 1500);
 	}
 }
 
-function updateDisplay(inputData){
-	var displayMessage = getValue(inputData).join(' ');
-	$('.screen').text(displayMessage);
+function updateStats(score) {
+	if (score%9 === 0 && score !== 0){
+		var gameWon = (score/9).toString();
+		$('#game .stats .games-played .value').text(gameWon);
+		updateAttempts(attempt_counter.toString());
+		updateAccuray((((score/attempt_counter)*100).toFixed(2)));
+		displayWin();
+	} 
 }
 
-function factorial(num) {
-	if (num >= 0) {
-		var result = 1;
-		for (var i = 2; i <= num; i++){
-			result *= i;
-		}
-		return result;
-	} else {
-		return 'Error';
+function updateAttempts(num) {
+	$('#game .stats .attempts .value').text((num).toString());
+}
+
+function updateAccuray(num) {
+	$('#game .stats .accuracy .value').text(num + '%');
+}
+
+function displayWin() {
+	setTimeout(function(){
+		$('.win_message.hide').removeClass('hide');
+		$('.win_message').css({
+			'background-image':'url(img/Smaug.gif)',
+			'background-position': 'center',
+			'background-repeat': 'no-repeat',
+			'background-size': 'cover'
+		});
+		sounds.roar.play();
+		setTimeout(function(){
+			$('.win_message span').text('You won');
+			$('.win_message span').addClass('animation_fadeIn');
+		}, 3000);
+		setTimeout(function(){
+			$('.win_message').css('background-image', '');
+		}, 2600);
+	}, 1500);
+	anotherClickAllowed = false;
+}
+
+function reset() {
+	$('#game-area').html('<div class="win_message hide"><span></span></div>');
+	setTable();
+	anotherClickAllowed = true;
+}
+
+function makeCards(cardArr) {
+	var game_area = $('#game-area');
+	for (var i = 0; i < cardArr.length; i++) {
+		var card = $('<div>').addClass('card');
+		var front = $('<div>').addClass('front');
+		var back = $('<div>').addClass('back');
+		var frontImg = $('<img>').attr('src', cardArr[i]);
+		var backImg	= $('<img>').attr('src', 'img/card-back.png');
+		front.append(frontImg);
+		back.append(backImg);
+		card.append(front);
+		card.append(back);
+		game_area.append(card);
 	}
 }
 
-function displayIntro() {
-	dataStorage = [];
-	inputHistory = [];
-	var intro = "Greetings! My name is Mathy McMathface. May I help you with some math?";
-	insert(dataStorage, intro, 1);
-	updateDisplay(dataStorage);
-	dataStorage = [];
-	insert(dataStorage);
+function shuffle(cards) {
+	for (var i = 0; i < cards.length; i++) {
+		var randomIndex = Math.floor(Math.random() * (i+1));
+		var buffer = cards[i];
+		cards[i] = cards[randomIndex];
+		cards[randomIndex] = buffer;
+	}
+	return cardArr;
 }
 
-function doMath(inputData) {
-	for (var i = 0; i < inputData.length; i++) {
-		if (inputData[i].rank === 5){
-			if (inputData[i].value === 'sin') {
-				inputData[i].value = Math.sin( inputData[i+1].value * Math.PI / 180).toFixed(6);
-			} else if (inputData[i].value === 'cos') {
-				inputData[i].value = Math.cos( inputData[i+1].value * Math.PI / 180).toFixed(6);
-			} else if (inputData[i].value === 'log'){
-				inputData[i].value = Math.log10(inputData[i+1].value);
-			} else if (inputData[i].value === 'factorial') {
-				inputData[i].value = factorial(inputData[i+1].value);
-			}
-			inputData[i].rank = 1;
-			inputData.splice(i+1, 1);
-			i -= 1;
-		// } else if (inputData[i].rank === 5 && !operators.includes(inputData[i-1].value)){
-		// 	inputData = [{
-		// 				value: 'Error'
-		// 	}];
-		}
-	}
-	for (var i = 0; i < inputData.length; i++) {
-		if (inputData[i].rank === 4){
-			if (inputData[i].value === 'power') {
-				inputData[i-1].value = Math.pow( inputData[i-1].value, inputData[i+1].value).toFixed(6);
-			} else if (inputData[i].value === 'root') {
-				inputData[i-1].value = Math.pow( inputData[i-1].value, 1/inputData[i+1].value).toFixed(6);
-			}
-			inputData.splice(i, 2);
-			i -= 2;
-		}
-	}
-	for (var i = 0; i < inputData.length; i++){
-		if (inputData[i].rank === 3){
-			if (inputData[i].value === '*'){
-				inputData[i-1].value *= inputData[i+1].value; 
-				inputData.splice(i, 2);
-			} else if (inputData[i].value === '/'){
-				if (parseFloat(inputData[i+1].value) === 0) {
-					inputData = [{
-						value: 'Error'
-					}];
-				} else {
-					inputData[i-1].value /= inputData[i+1].value; 
-					inputData[i-1].value = Number(inputData[i-1].value).toFixed(6);
-					inputData.splice(i, 2);
-				}
-			}
-			i -= 2;
-		}
-	}
-	for (var i = 0; i < inputData.length; i++) {
-		if (inputData[i].rank === 2) {
-			if (inputData[i].value === '+') {
-				inputData[i-1].value = (parseFloat(inputData[i-1].value) + parseFloat(inputData[i+1].value)).toFixed(6);
-			}
-			if (inputData[i].value === '-') {
-				inputData[i-1].value -= inputData[i+1].value; 
-			}
-			inputData.splice(i, 2);
-			i -= 2;
-		}
-	}
-	return inputData;
+function startCutScene() {
+	$('.win_message').removeClass('hide');
+	$('.win_message').css({
+			'background-image':'url(img/flyover.gif)',
+			'background-repeat': 'no-repeat',
+			'background-size': 'cover',
+			'background-position': 'center'
+		});
+	sounds.wing.play();
+	setTimeout(function(){
+		$('.win_message').addClass('hide');
+		}, 4200);
+}
+
+var sounds = {
+	roar: new Howl({
+		src: ['sounds/roar.mp3'],
+		volume: 0.25
+	}),
+	wing: new Howl({
+		src: ['sounds/wingflap.mp3'],
+		volume: 1
+	})
 }
